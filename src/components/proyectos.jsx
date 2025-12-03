2d98ab9d7f8dbee7e0d9d90bbbdcd97f4ae921dd
4Retr00 <afp00@iesemilidarder.com>
2025-12-03 18:40:43 +0100

    cambios en los poryectos

--- src/components/proyectos.jsx
+++ src/components/proyectos.jsx
@@ -5,18 +5,18 @@ export default function Proyectos() {
                 <h1 className="titulo1">Trabajos</h1>
                 <div className="trabajo">
                     <a href="https://academia-de-hackers-retr0.netlify.app" className="trabajo1" target="_blank">
-                    <h1>Academia Hackers</h1>
-                    <img src="./assets/img/academiahackers.png" alt=""/>
+                    <h1>React Notes</h1>
+                    <img src="./assets/img/react_notes.png" alt=""/>
                     </a>
 
                     <a href="https://porta-aventura-retr0.netlify.app" className="trabajo1" target="_blank">
-                        <h1>Porta Aventura</h1>
-                        <img src="./assets/img/portaaventura.png" alt=""/>
+                        <h1>Rocket Store</h1>
+                        <img src="./assets/img/rocket_store.png" alt=""/>
                     </a>
 
                     <a href="https://3djuegos-retr0.netlify.app" className="trabajo1" target="_blank">
-                        <h1>3D Juegos</h1>
-                        <img src="./assets/img/3djuegos.png" alt=""/>
+                        <h1>Out Tours</h1>
+                        <img src="./assets/img/tours.png" alt=""/>
                     </a>
                 </div>
             </div>
