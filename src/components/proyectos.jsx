971f2ea28081f14b57a63e56d7c1abdbefb3e708
4Retr00 <afp00@iesemilidarder.com>
2025-12-09 16:44:58 +0100

    cambios

--- src/components/proyectos.jsx
+++ src/components/proyectos.jsx
@@ -4,7 +4,7 @@ export default function Proyectos() {
             <div className="webs" id="webs" >
                 <h1 className="titulo1">Proyectos</h1>
                 <div className="trabajo">
-                    <a href="https://academia-de-hackers-retr0.netlify.app" className="trabajo1" target="_blank">
+                    <a href="https://react-notes-afp.netlify.app/" className="trabajo1" target="_blank">
                     <h1>React Notes</h1>
                     <img src="./assets/img/react_notes.png" alt=""/>
                     </a>
