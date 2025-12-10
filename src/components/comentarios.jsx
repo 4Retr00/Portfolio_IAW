391ced4460598660344eb7cbd10235c2737da239
4Retr00 <afp00@iesemilidarder.com>
2025-12-10 20:48:34 +0100

    cambios

--- src/components/comentarios.jsx
+++ src/components/comentarios.jsx
@@ -1,48 +1,34 @@
+import { useEffect, useState } from "react";
+
 export default function Comentarios() {
-    return(
+
+    const [comentarios, setComentarios] = useState([]);
+
+    useEffect(() => {
+        fetch("data/comentarios.json")  // ← ruta correcta
+            .then(res => res.json())
+            .then(data => setComentarios(data.comentarios));
+    }, []);
+
+    return (
         <main>
             <div className="comentarios" id="comentarios">
                 <h1>Lo que dice la gente</h1>
+
                 <div className="coment">
-
-                    <div className="card">
-                        <div className="header">
-                            <img src="./assets/img/coment01.jpg" alt=""/>
-                            <p className="message">Tu página destaca con un diseño visualmente impactante</p>
+                    {comentarios.map(persona => (
+                        <div className="card" key={persona.id}>
+                            <div className="header">
+                                <img src={persona.foto} alt={persona.nombre} />
+                                <p className="message">{persona.mensaje}</p>
+                            </div>
+                            <h2 className="name">{persona.nombre}</h2>
+                            <h4 className="name2">{persona.Cargo}</h4>
                         </div>
-                        <h2 className="name">Ben</h2>
-                        <h4 className="name2">Director de Arte</h4>
-                    </div>
-
-                    <div className="card">
-                        <div className="header">
-                            <img src="./assets/img/coment02.jpg" alt=""/>
-                            <p className="message">La página es fácil sumergirse fácilmente en el portafolio.</p>
-                        </div>
-                        <h2 className="name">Maria</h2>
-                        <h4 className="name2">Consultora de Marketing</h4>
-                    </div>
-
-                    <div className="card">
-                        <div className="header">
-                            <img src="./assets/img/coment03.jpg" alt=""/>
-                            <p className="message">La navegación es intuitiva, para los visitantes explorar sin esfuerzo</p>
-                        </div>
-                        <h2 className="name">Jen</h2>
-                        <h4 className="name2">Manager</h4>
-                    </div>
-
-                    <div className="card">
-                        <div className="header">
-                            <img src="./assets/img/coment04.jpg" alt=""/>
-                            <p className="message">La página impresiona con su diseño atractivo.</p>
-                        </div>
-
-                        <h2 className="name">Wilson</h2>
-                        <h4 className="name2">Desarrollador Web</h4>
-                    </div>
+                    ))}
                 </div>
+
             </div>
         </main>
-    )
+    );
 }
