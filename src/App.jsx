f7636fec770f2b49a92d40c73857348b77695e9e
4Retr00 <afp00@iesemilidarder.com>
2025-12-11 17:44:33 +0100

    a

--- src/App.jsx
+++ src/App.jsx
@@ -3,17 +3,16 @@ import Newsletter from "./components/newsletter/newsletter.jsx";
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import "./css/styles.css";
 
+
 export default function App() {
-  return(
-    <>
-      <BrowserRouter>
-        <Routes>
-            <Route path="/" element={<Navbar/>}/>
-            <Route path="/newsletter" element={<Newsletter/>}/>
-        </Routes>
-      </BrowserRouter>
+  return (
+    <BrowserRouter>
 
-    </>
-  )
+      {/* Contenido que cambia según la ruta */}
+      <Routes>
+        <Route path="/" element={<Navbar />} />
+        <Route path="/newsletter" element={<Newsletter />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
-
