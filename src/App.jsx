df20c423c4f29614d0b45f5a7c2fe749b1e737eb
4Retr00 <afp00@iesemilidarder.com>
2025-12-11 17:36:11 +0100

    cambios

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import Navbar from "./components/navbar.jsx";
-import Newsletter from "./components/newsletter/newsletter.jsx";
+import Newsletter from "./components/newsletter.jsx";
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import "./css/styles.css";
 
